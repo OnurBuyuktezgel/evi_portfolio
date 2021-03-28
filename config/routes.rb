fdf02c12c950b541ad332bf9f4d42985f0439940
OB <obuyuktezgel@gmail.com>
2021-03-28 16:15:15 +0200

    created about page, added links to the navbar

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lectures, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
+    resources :bookings, only: [:index, :show, :new, :create]
+  end
+  resources :bookings, only: [:destroy]
 
   resources :contacts, only: [:new, :create]
+
+  get "about", to: 'pages#about'
 end
