fcc2d756ca68fb5109c9099bb0eee1bfb008961a
OB <obuyuktezgel@gmail.com>
2021-03-30 16:34:10 +0200

    added sticky navbar and improved css

--- app/javascript/components/scroll.js
+++ app/javascript/components/scroll.js
@@ -7,7 +7,7 @@ const scrollFunc = () => {
   let y = window.scrollY;
 
   // If the scroll value is greater than the window height, let's add a class to the scroll-to-top button to show it!
-  if (y > 0) {
+  if (y > 0.5) {
     scrollToTopButton.className = "top-link show";
   } else {
     scrollToTopButton.className = "top-link hide";
