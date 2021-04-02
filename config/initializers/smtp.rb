92029a692ae628d373867d26bb9efa72707ed61c
OB <obuyuktezgel@gmail.com>
2021-04-02 13:06:31 +0200

    fixed SENDGRID

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  address: ENV['SENDGRID_SMTP_SERVER'],
-  port: 465,
-  domain: 'evisanoll.com',
   user_name: ENV['SENGGRID_USERNAME'],
   password: ENV['SENDGRID_PASSWORD'],
-  authentication: :login,
+  domain: 'evisanoll.com',
+  address: ENV['SENDGRID_SMTP_SERVER'],
+  port: 587,
+  authentication: :plain,
   enable_starttls_auto: true
 }
