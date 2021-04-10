04adf161d6a1c19192d9fe28d79f862a3d5cc93b
OB <obuyuktezgel@gmail.com>
2021-04-10 22:49:34 +0200

    removed skip authentication before booking show

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -1,5 +1,5 @@
 class LecturesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
+  skip_before_action :authenticate_user!, only: [:index]
   before_action :set_lecture, only: [:show, :destroy, :edit, :update]
   before_action :set_user, only: [:index, :new, :show, :edit, :update]
 
